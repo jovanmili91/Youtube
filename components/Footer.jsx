4e9b67b909c4bd4dffa3dd0760f114d0e0b7415a
Jovan Milivojevic <jovanmili91@gmail.com>
2022-08-02 13:22:03 +0200

    Completed

---
+++ components/Footer.jsx
@@ -0,0 +1,23 @@
+import { UilFacebook, UilGithub, UilInstagram } from "@iconscout/react-unicons";
+import Image from "next/image";
+import Logo from "../assets/logo.png";
+
+import css from "../styles/Footer.module.css";
+export default function Footer() {
+  return (
+    <div>
+      <div className={css.container}>
+        <span>ALL RIGHT RESERVED</span>
+        <div className={css.social}>
+          <UilFacebook size={45} />
+          <UilGithub size={45} />
+          <UilInstagram size={45} />
+        </div>
+        <div className={css.logo}>
+          <Image src={Logo} alt="" width={50} height={50} />
+          <span>Fudo</span>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ components/Header.jsx
@@ -0,0 +1,71 @@
+import css from "../styles/Header.module.css";
+import Image from "next/image";
+import Logo from "../assets/logo.png";
+import { UilShoppingBag, UilReceipt } from "@iconscout/react-unicons";
+import { useStore } from "../store/store";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { FaBars } from "react-icons/fa";
+
+export default function Header() {
+  const [menuOpened, setmenuOpened] = useState(false);
+  const [Order, setOrder] = useState("");
+  const [width, setWidth] = useState(0);
+
+  const mobile = width <= 768 ? true : false;
+
+  useEffect(() => {
+    setOrder(localStorage.getItem("order"));
+    setWidth(window.innerWidth);
+  }, []);
+
+  const items = useStore((state) => state.cart.pizzas.length);
+
+  return (
+    <div className={css.header}>
+      {/* logo side */}
+      <Link href="../">
+        <div className={css.logo}>
+          <Image src={Logo} alt="" width={50} height={50} />
+          <span>Lady S - Accessories </span>
+        </div>
+      </Link>
+      {/* menu side */}
+
+      {menuOpened === false && mobile === true ? (
+        <div></div>
+      ) : (
+        <ul className={css.menu}>
+          <li onClick={() => setmenuOpened(false)}>
+            <Link href="../">Home</Link>
+          </li>
+          <li onClick={() => setmenuOpened(false)}>
+            <Link href="#menu">Menu</Link>
+          </li>
+          <li onClick={() => setmenuOpened(false)}>Contact</li>
+        </ul>
+      )}
+
+      {/* right side */}
+      <div className={css.rightSide}>
+        <Link href="/cart">
+          <div className={css.cart}>
+            <UilShoppingBag size={35} color="#2E2E2E" />
+            <div className={css.badge}>{items}</div>
+          </div>
+        </Link>
+        {Order && (
+          <Link href={`/order/${Order}`}>
+            <div className={css.cart}>
+              <UilReceipt size={35} color="#2E2E2E" />
+              {Order != "" && <div className={css.badge}>1</div>}
+            </div>
+          </Link>
+        )}
+        <div className={css.bars} onClick={() => setmenuOpened(true)}>
+          <FaBars />
+        </div>
+      </div>
+    </div>
+  );
+}
